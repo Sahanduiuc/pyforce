3631bfe6056794daf7a7833f34bc228ba8c7b0ae
cswaney <colinswaney@gmail.com>
2017-02-11 13:34:45 -0600

    Working example?

--- pyforce/core.py
+++ pyforce/core.py
@@ -3,42 +3,77 @@ import h5py as h5
 import numpy as np
 import gym
 
-# TODO: How to deal with action spaces that depends on the state?
-# TODO: Let the network keep track of alpha?
+# NOTE:
 
-def gain(rewards, start=0, gamma=1.0):
-    R = rewards[start + 1:]
-    d = gamma ** np.arange(0, len(R))
-    return np.sum(R * d)
+# TODO:
 
-def progress(current, maximum, step_ratio=0.05):
-    check = current % int(maximum * step_ratio)
-    if check == 0:
-        stars = int(current / int(maximum * 0.05))
-        spaces = int(1 / step_ratio) - stars
-        if stars < int(1 / step_ratio):
-            bar = "\tProgress: " + "#" * stars + " " * spaces + " ({:.2f})"
-            ratio = stars / int(1 / step_ratio)
-            print(bar.format(ratio), end="\r")
-        else:
-            print("\tProgress: " + "#" * stars + " (1.00) - Done", end="\n")
+NN = 512
+NUM_FEATS = 4
+NUM_ACTIONS = 2
+MAX_EPISODES = 500
+LEARNING_RATE = 0.0001
+DECAY_STEPS = 1000
+DECAY_FACTOR = 0.90
+
+def placeholders():
+    num_features = 4
+    num_actions = 2
+    state_pl = tf.placeholder(tf.float32, shape=(1, num_features))
+    action_pl = tf.placeholder(tf.int32, shape=(num_actions, 1))
+    gain_pl = tf.placeholder(tf.float32, shape=())
+    prob_pl = tf.placeholder(tf.float32, shape=())
+    return state_pl, action_pl, gain_pl, prob_pl
+
+def activation(features, keep_prob):
+    """Returns probability of each action."""
+    nn = NN
+    num_features = NUM_FEATS
+    num_actions = NUM_ACTIONS
+    with tf.name_scope('full'):
+        sd = 1.0 / np.sqrt(float(num_features))
+        weights = tf.Variable(tf.truncated_normal([num_features, nn], stddev=sd), name='weights')
+        bias = tf.Variable(tf.constant(0.1, shape=[nn]), name='bias')
+        full =  tf.nn.dropout(tf.nn.relu(tf.matmul(features, weights) + bias), keep_prob)
+    with tf.name_scope('softmax'):
+        sd = 1.0 / np.sqrt(float(nn))
+        weights = tf.Variable(tf.truncated_normal([nn, num_actions], stddev=sd), name='weights')
+        bias = tf.Variable(tf.constant(0.0, shape=[num_actions]), name='bias')
+        logits =  tf.matmul(full, weights) + bias
+    return logits
+
+def eligibility(logits, action, gain):
+    eligibility = tf.log(tf.matmul(tf.nn.softmax(logits), tf.cast(action, tf.float32)))
+    loss = - gain * eligibility
+    # tf.summary.scalar('eligibility', eligibility)
+    return loss
+
+def update(loss, lr, global_step, decay_steps, decay_factor):
+    """Peforms modified (RMSProp) REINFORCE update."""
+    lr = tf.train.exponential_decay(lr, global_step, decay_steps, decay_factor)
+    # tf.summary.scalar('learning_rate', lr)
+    optimizer = tf.train.GradientDescentOptimizer(lr)
+    return optimizer.minimize(loss, global_step=global_step)
 
 class Network():
-    """A TensorFlow neural network with methods for reinforcement learning.
 
-       The network should be thought of as a network for classification with N classes.
-       In this case the N classes are N possible actions in the action space.
-    """
-
-    def __init__(self, eval_op, weights, actions, gradient, update_op, session, placeholders, init):
-        self.eval_op = eval_op
-        self.weights = weights
+    def __init__(self, actions, keep_prob):
         self.actions = actions
-        self.gradient = gradient
-        self.update_op = update_op
-        self.session = session
-        self.placeholders = placeholders  # {'state_pl': state_pl, ..., 'alpha_pl': alpha_pl}
-        self.init = init
+        self.keep_prob = keep_prob
+        with tf.Graph().as_default():
+            self.global_step = tf.Variable(0, trainable=False, name="global_step")
+            self.features_pl = tf.placeholder(tf.float32, shape=(1, 4))
+            self.action_pl = tf.placeholder(tf.float32, shape=(2, 1))
+            self.gain_pl = tf.placeholder(tf.float32, shape=())
+            self.prob_pl = tf.placeholder(tf.float32, shape=())
+            self.logits = activation(self.features_pl, self.prob_pl)
+            self.loss = eligibility(self.logits, self.action_pl, self.gain_pl)
+            self.train_op = update(self.loss, LEARNING_RATE, self.global_step, DECAY_STEPS, DECAY_FACTOR)
+            self.init_op = tf.global_variables_initializer()
+            self.sess = tf.Session()
+            # self.summarizer = tf.summary.merge_all()
+            # self.saver = tf.train.Saver()
+            # self.writer = tf.summary.FileWriter(log_dir, self.sess.graph)
+            self.sess.run(self.init_op)
 
     def __str__(self):
         return "Network()"
@@ -46,56 +81,63 @@ class Network():
     def __repr__(self):
         return "<pf.Network>"
 
-    def initialize(self):
-        self.session.run([self.init])
+    def close(self):
+        self.sess.close()
 
     def evaluate(self, s, a=None):
-        """Network evaluated at s, then a."""
-
-
-        state_pl = self.placeholders['state_pl']
-        output = self.session.run(self.eval_op, feed_dict={state_pl:s})
+        """Network evaluated at s, then (optionally) at a."""
+        # state_pl = self.placeholders['state_pl']
+        output = self.sess.run(self.logits, feed_dict={self.features_pl:s,
+                                                       self.prob_pl:self.keep_prob})
         if a is None:
             return output
         else:
             a_idx = self.actions.index(a)
             return output[0, a_idx]
 
-    def differentiate(self, s, a):
-        """Gradient of the network evaluated at s, then a."""
-        state_pl = self.placeholders['state_pl']
-        g = self.session.run(self.gradient, feed_dict={state_pl:s})
-        return g[self.actions.index(a)]
-
-    def update(self, a, s, g, p, alpha):
-        a_idx = self.actions.index(a)
-        feed_dict = {self.placeholders['state_pl']:s,
-                     self.placeholders['gain_pl']:g,
-                     self.placeholders['value_pl']:p,
-                     self.placeholders['alpha_pl']:alpha}
-        self.session.run(self.update_op[a_idx], feed_dict=feed_dict)
+    def train(self, s, a, g):
+        feed_dict = {self.features_pl:s,
+                     self.action_pl:a,
+                     self.gain_pl:g,
+                     self.prob_pl:self.keep_prob}
+        self.sess.run(self.train_op, feed_dict=feed_dict)
 
     def choice(self, s):
         """Sample an action from probabilities given by forward pass of s."""
-        a = np.array(self.actions)
-        k, = a.shape
-        return np.random.choice(a, p=self.evaluate(s).reshape(k,))
+        y = self.evaluate(s)
+        p = np.exp(y) / np.sum(np.exp(y))
+        return np.random.choice(self.actions, p=p.reshape(len(self.actions),))
 
-    def get_weights(self):
-        return self.session.run(self.weights)
+    def probabilities(self, s):
+        y = self.evaluate(s)
+        return np.exp(y) / np.sum(np.exp(y))
 
-    def set_weights(self):
-        pass
+def gain(rewards, start=0, gamma=1.0):
+    R = rewards[start + 1:]
+    d = gamma ** np.arange(0, len(R))
+    return np.sum(R * d)
+
+def progressBar(current, maximum, step_ratio=0.05):
+    check = current % int(maximum * step_ratio)
+    if check == 0:
+        stars = int(current / int(maximum * 0.05))
+        spaces = int(1 / step_ratio) - stars
+        if stars < int(1 / step_ratio):
+            bar = "Progress: " + "#" * stars + " " * spaces + " ({}/{})"
+            print(bar.format(current, maximum), end="\r")
+        else:
+            bar = "Progress: " + "#" * stars + " ({}/{}) - Done"
+            print(bar.format(current, maximum), end="\n")
 
 class Learner():
     """Combines network and environment to implement training."""
 
-    def __init__(self, network, env, max_episodes, alpha, discount=1.0):
-        self.network = network  # a Keras model
+    def __init__(self, net, env, discount_rate, score):
+        self.net = net  # a Keras model
         self.env = env  # a Gym environment
-        self.max_episodes = max_episodes
-        self.alpha = alpha
-        self.discount = discount
+        self.discount_rate = discount_rate
+        self.score = score
+        self.scores = []
         self.states = []
         self.actions = []
         self.rewards = []
@@ -117,8 +159,8 @@ class Learner():
 
 class Reinforce(Learner):
 
-    def __init__(self, network, env, max_episodes, alpha, discount=1.0):
-        Learner.__init__(self, network, env, max_episodes, alpha, discount)
+    def __init__(self, net, env, discount_rate, score):
+        Learner.__init__(self, net, env, discount_rate, score)
 
     def __str__(self):
         s = format(env).rstrip(" instance>") + ">"
@@ -131,6 +173,7 @@ class Reinforce(Learner):
     def step(self):
         """Perform one step of the learning algorithm (one episode for REINFORCE)."""
 
+        # Generate an episode
         self.env.reset()
         self.states = []
         self.actions = []
@@ -140,26 +183,38 @@ class Reinforce(Learner):
         steps = 0
         while not done:
             self.states.append(s)
-            a = self.network.choice(s)
+            a = self.net.choice(s.reshape(1,4))
             s, r, done, info = self.env.step(a)
             steps += 1
             self.actions.append(a)
             self.rewards.append(r)
             if done:
                 self.episodes += 1
+
+        # Perform REINFORCE updates
         for t in np.arange(0, steps):
             s = self.states[t]
-            a = self.actions[t]
-            a_idx = self.network.actions.index(a)
-            g = gain(self.rewards, start=t, gamma=self.discount)
-            p = self.network.evaluate(s, a)
-            self.network.update(a, s, g, p, self.alpha * self.discount ** t)
+            a_idx = self.net.actions.index(self.actions[t])
+            a = np.zeros((len(self.net.actions), 1))
+            a[a_idx, 0] = 1
+            g = gain(self.rewards, start=t, gamma=self.discount_rate) * self.discount_rate ** t
+            self.net.train(s.reshape(1,4), a, g)
+
+        return self.score((self.rewards))
 
     def train(self):
         self.episodes = 0
-        while self.episodes < self.max_episodes:
-            self.step()
-            progress(self.episodes, self.max_episodes)
+        self.scores = []
+        progressBar(self.episodes, MAX_EPISODES)
+        while self.episodes < MAX_EPISODES:
+            score = self.step()
+            self.scores.append(score)
+            progressBar(self.episodes, MAX_EPISODES)
+
+    def summary(self):
+        if len(self.scores) > 0:
+            x = np.array(self.scores)
+            x.describe()
 
 class Sarsa(Learner):
 
--- pyforce/example.py
+++ pyforce/example.py
@@ -1,88 +1,8 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 import core as pf
 import gym
-
-nn = 64
-k = 4
-action_space = [0,1]
-discount = 0.99
-alpha = 0.0001
-max_episodes = 10000
-
-def shape(tensor):
-    """Returns the shape of the tensor as a tuple. A more useful alternative to
-       tf.get_shape()."""
-    shape = tensor.get_shape()
-    return tuple([shape[i].value for i in range(0, len(shape))])
-
-def placeholders(batch_size, num_features):
-    features_pl = tf.placeholder(tf.float32, shape=[batch_size, num_features])
-    labels_pl = tf.placeholder(tf.int32, shape=[batch_size, ])
-    return features_pl, labels_pl
-
-def feed(data, features_pl, labels_pl, batch_size):
-    """Feed the next batch of data to placeholders.
-
-    Arguments
-    ---------
-    data (dict): datasets to get batches from.
-
-    Returns
-    -------
-    A dictionary of placeholders (key) and associated data (value).
-
-    """
-
-    features = data['features'].get_batch(batch_size, demean=True)
-    labels = data['labels'].get_batch(batch_size)
-    feed_dict = {features_pl: features, labels_pl: labels}
-    return feed_dict
-
-def activation(features_pl):
-    k, = shape(features_pl)
-    x = tf.reshape(state_pl, shape=(1,k))
-    with tf.name_scope('full'):
-        sd = 1.0 / np.sqrt(float(k))
-        weights = tf.Variable(tf.truncated_normal([k, nn], stddev=sd), name='weights')
-        bias = tf.Variable(tf.constant(0.0, shape=[nn]), name='bias')
-        full =  tf.matmul(x, weights) + bias
-    with tf.name_scope('softmax'):
-        sd = 1.0 / np.sqrt(float(nn))
-        weights = tf.Variable(tf.truncated_normal([nn, 2], stddev=sd), name='weights')
-        bias = tf.Variable(tf.constant(0.0, shape=[2]), name='bias')
-        logits =  tf.matmul(full, weights) + bias
-        pi = tf.nn.softmax(logits)
-    return pi
-
-def gradient(y):
-    _, k = shape(y)
-    G = [tf.gradients(indicator(y, i), weights) for i in np.arange(0, k)]
-    return G
-
-def updates(weights, gradients, value, gain, alpha):
-    """This will be added to the graph at the same time as the weight and gradient
-       tensors. Also need to add gain, value, and alpha placeholders to the graph.
-       Then this operation can be called by running sess.run(update, feed_dict=...).
-    """
-
-    ops = []
-    for Df in gradients:
-        sub_ops = []
-        for w,df in zip(weights, Df):
-            # print("added an update op")
-            sub_ops.append(tf.assign(w, tf.add(w, tf.multiply(alpha, tf.multiply(gain, tf.divide(df, value))))))
-        ops.append(sub_ops)
-    return ops
-
-def indicator(tensor, index):
-    _, k = tensor.get_shape().as_list()  # assume it is (num_examples x k)
-    s = np.zeros([1, k])
-    s[0, index] = 1
-    return tensor * s
-
-def softmax(logits):
-    return np.exp(logits) / np.sum(np.exp(logits))
 
 def run_episode(env, render=False, learner=None):
     s = env.reset()
@@ -92,43 +12,30 @@ def run_episode(env, render=False, learner=None):
         if render:
             env.render()
         if learner:
-            action = learner.network.choice(s)
+            action = learner.net.choice(s.reshape(1,4))
         else:
             action = env.action_space.sample()
         s, r, done, info = env.step(action)
         steps += 1
     return steps
 
-# Initialize the network
-with tf.Graph().as_default():
-    state_pl = tf.placeholder(tf.float32, shape=(k,))
-    gain_pl = tf.placeholder(tf.float32, shape=())
-    alpha_pl = tf.placeholder(tf.float32, shape=())
-    value_pl = tf.placeholder(tf.float32, shape=())
-    placeholders = {'state_pl': state_pl,
-                    'gain_pl': gain_pl,
-                    'alpha_pl': alpha_pl,
-                    'value_pl': value_pl}
-    eval_op = activation(state_pl)
-    weights = tf.get_collection('trainable_variables')  # must come AFTER eval_op!
-    diff_op = gradient(eval_op)
-    update_op = updates(weights, diff_op, value_pl, gain_pl, alpha_pl)
-    init = tf.global_variables_initializer()
-    sess = tf.Session()
-network = pf.Network(eval_op, weights, action_space, diff_op, update_op, sess, placeholders, init)
-network.initialize()
+def score(rewards):
+    """Score for the cart pole environment."""
+    return len(rewards)
 
-# Initialize the environment
+KEEP_PROB = 1.00
+ACTIONS = [0,1]
+DISCOUNT_RATE = 1.00
+
+net = pf.Network(actions=ACTIONS, keep_prob=KEEP_PROB)
+
 env = gym.make('CartPole-v0')
 
-# Initialize the learner
-learner = pf.Reinforce(network=network, env=env, max_episodes=max_episodes, alpha=alpha)
+learner = pf.Reinforce(net=net, env=env, discount_rate=DISCOUNT_RATE, score=score)
 
-# Train the learner
 learner.train()
 
-# Test the learner
-episodes = 1000
+episodes = 100
 reinforce = 0
 random = 0
 for _ in range(episodes):
@@ -136,3 +43,5 @@ for _ in range(episodes):
     random += run_episode(env, render=False)
 print("REINFORCE average = {}".format(reinforce / episodes))
 print("Random average = {}".format(random / episodes))
+plt.plot(learner.scores)
+plt.show()
